5ed7e1257558f12b6bb904362e9fda694e364966
dezkareid <elmaildeldezkareid@gmail.com>
2014-02-23 09:09:51 -0600

    JS alerta y reporte

--- amberMx/www/js/alerta.js
+++ amberMx/www/js/alerta.js
@@ -24,21 +24,4 @@ var l, ln;
           alert('dragend');
         }
       });
-       
-       
-      //
-     /* GMaps.geolocate({
-        success: function(position){
-          map.setCenter(position.coords.latitude, position.coords.longitude);
-        },
-        error: function(error){
-          alert('Geolocation failed: '+error.message);
-        },
-        not_supported: function(){
-          alert("Your browser does not support geolocation");
-        },
-        always: function(){
-          alert("Done!");
-        }
-      });*/
     });
