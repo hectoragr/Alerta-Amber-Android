15860e7e47c18e1a9498ecd623790265739cbc60
dezkareid <elmaildeldezkareid@gmail.com>
2014-02-23 07:52:45 -0600

    full pic mejorado map

--- amberMx/www/js/alerta.js
+++ amberMx/www/js/alerta.js
@@ -1,3 +1,35 @@
-$( document ).ready(function() {
-  
-});
+var map;
+    $(document).ready(function(){
+     // prettyPrint();
+      map = new GMaps({
+        div: '#map',
+        lat: -12.043333,
+        lng: -77.028333,
+         click: function (results, status){
+         	alert();
+            if(status=='OK'){
+              var latlng = results[0].geometry.location;
+              map.setCenter(latlng.lat(), latlng.lng());
+              alert(latlng);
+            }
+        },
+        dragend: function(e){
+          alert('dragend');
+        }
+      });
+      //
+     /* GMaps.geolocate({
+        success: function(position){
+          map.setCenter(position.coords.latitude, position.coords.longitude);
+        },
+        error: function(error){
+          alert('Geolocation failed: '+error.message);
+        },
+        not_supported: function(){
+          alert("Your browser does not support geolocation");
+        },
+        always: function(){
+          alert("Done!");
+        }
+      });*/
+    });
