b07cb0f89345b946531e1197b0de3092bfd04f07
Héctor Gómez <hector.agr@gmail.com>
2014-02-23 09:12:43 -0600

    Mapas

--- amberMx/platforms/android/assets/www/js/alerta.js
+++ amberMx/platforms/android/assets/www/js/alerta.js
@@ -1,3 +1,44 @@
-$( document ).ready(function() {
-  
-});
+var map;
+var marker;
+var l, ln;
+    $(document).ready(function(){
+     // prettyPrint();
+      map = new GMaps({
+        div: '#map',
+        lat: -12.043333,
+        lng: -77.028333,
+         click: function(e){
+           console.log(e.latLng);
+           l = e.latLng.lat();
+           ln = e.latLng.lng();
+           map.addMarker({
+        lat: l,
+        lng: ln,
+        title: 'Marker with InfoWindow',
+        infoWindow: {
+          content: '<p>HTML Content</p>'
+        }
+      });
+        },
+        dragend: function(e){
+          alert('dragend');
+        }
+      });
+       
+       
+      //
+     /* GMaps.geolocate({
+        success: function(position){
+          map.setCenter(position.coords.latitude, position.coords.longitude);
+        },
+        error: function(error){
+          alert('Geolocation failed: '+error.message);
+        },
+        not_supported: function(){
+          alert("Your browser does not support geolocation");
+        },
+        always: function(){
+          alert("Done!");
+        }
+      });*/
+    });
