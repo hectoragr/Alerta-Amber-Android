06d7037b22196130f6d836522cad5118609b5145
dezkareid <elmaildeldezkareid@gmail.com>
2014-02-23 08:37:57 -0600

    JS alerta y reporte

--- amberMx/www/js/alerta.js
+++ amberMx/www/js/alerta.js
@@ -1,22 +1,31 @@
 var map;
+var marker;
+var l, ln;
     $(document).ready(function(){
      // prettyPrint();
       map = new GMaps({
         div: '#map',
         lat: -12.043333,
         lng: -77.028333,
-         click: function (results, status){
-         	alert();
-            if(status=='OK'){
-              var latlng = results[0].geometry.location;
-              map.setCenter(latlng.lat(), latlng.lng());
-              alert(latlng);
-            }
+         click: function(e){
+           console.log(e.latLng);
+           l = e.latLng.lat();
+           ln = e.latLng.lng();
+           map.addMarker({
+        lat: l,
+        lng: ln,
+        title: 'Marker with InfoWindow',
+        infoWindow: {
+          content: '<p>HTML Content</p>'
+        }
+      });
         },
         dragend: function(e){
           alert('dragend');
         }
       });
+       
+       
       //
      /* GMaps.geolocate({
         success: function(position){
